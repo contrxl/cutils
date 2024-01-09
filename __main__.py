8c7cac93a63cd0ee2371d1235b4ef72340870462
contrxl <contrxl@protonmail.com>
2024-01-09 12:02:00 +0000

    added url decoding, encoding setup to work but to be added

---
+++ __main__.py
@@ -0,0 +1,24 @@
+'''
+author: contrxl
+date: jan 8 2024
+version: 0.1
+'''
+
+from commands import decode
+from commands import encode
+from commands import defang
+import click
+import base64
+
+@click.group(help="A program to decode or encode strings.")
+@click.version_option("0.1.0", prog_name="cyutils")
+
+def cli():
+	pass
+
+cli.add_command(decode.decode)
+cli.add_command(encode.encode)
+cli.add_command(defang.defang)
+
+if __name__ == '__main__':
+	cli()
--- commands/decode.py
+++ commands/decode.py
@@ -5,19 +5,24 @@ version: 0.1
 '''
 import click
 import base64
+import urllib.parse
 
 @click.command()
+@click.argument("mode", type=click.STRING)
 @click.argument("string", type=click.STRING)
 @click.option("-o", "--output", type=click.STRING, help="Output string to a file called decoded.txt in the current directory.")
 @click.option("-p", "--path", type=click.File(mode="r"), help="Provide a file path to be read and decoded.")
 
-def decode(string, output, path):
+def decode(string, mode, output, path):
 	"""This will decode any provided string."""
-	if output:
-		file_object = open(output + ".txt", "w")
-		b64_out = (base64.b64decode(string).decode('utf-8'))
-		b64_file = file_object.write("String decodes to: " + b64_out)
-	else:
-		b64_out = (base64.b64decode(string).decode('utf-8'))
-		print("String decodes to: " + b64_out)
+	if mode == "b64":
+		if output:
+			file_object = open(output + ".txt", "w")
+			b64_out = (base64.b64decode(string).decode('utf-8'))
+			b64_file = file_object.write("String decodes to: " + b64_out)
+		else:
+			b64_out = (base64.b64decode(string).decode('utf-8'))
+			print("String decodes to: " + b64_out)
+	if mode == "url":
+		url_transl = print(urllib.parse.unquote(string))
 	
